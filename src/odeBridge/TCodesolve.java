b265e9666386964cd8c5005ee297a0b4573b376a
Chuan Fu Yap <chuanfuyap@BioLinux-V2>
2017-08-27 19:49:32 +0100

    optimised GA and Steady Solver

--- src/odeBridge/TCodesolve.java
+++ src/odeBridge/TCodesolve.java
@@ -35,7 +35,7 @@ public class TCodesolve {
         reactionID2=rID2;
     }
     public interface soslibbridge extends Library{
-        soslibbridge bridge = (soslibbridge) Native.loadLibrary("/home/chuanfuyap/NetBeansProjects/SOSlibJNIC/dist/SOSlibJNIC.so", soslibbridge.class);
+        soslibbridge bridge = (soslibbridge) Native.loadLibrary("./lib/SOSLib/dist/sosLibLinkv3.so", soslibbridge.class);
         
 //        ByValue runn(String link,Pointer vals, int numVals, Pointer newparam, Pointer names,  int parametercount, Pointer rID, int time);
         void TCrunSolver(String link,Pointer vals, int numVals, Pointer newparam, Pointer names,  int parametercount, Pointer rID, int rIDsize, Pointer rID2, Pointer time, int last, PointerByReference RowArray, IntByReference RowCount);
